4240e5ed84ec6168825c97adb0afc8697d171f3a
Saso Petrovski <techabyte@users.noreply.github.com>
2016-07-06 16:47:03 +1000

    Update balkanmp3.user.js

--- balkanmp3.user.js
+++ balkanmp3.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 //
-// @name           balkanmp3 Downloader
-// @description    Easy Balkanmp3 Downloader
-// @namespace      https://github.com/techabyte/
-// @author         techabyte (https://github.com/techabyte)
-// @license        GNU GPL v3 (http://www.gnu.org/copyleft/gpl.html)
-// @homepage       https://github.com/techabyte/
-// @include        http://balkanmp3.ba*
-// @grant       none
-// @require        https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js
+// @name            balkanmp3 Downloader
+// @description     Easy Balkanmp3 Downloader
+// @namespace       https://github.com/techabyte/
+// @author          techabyte (https://github.com/techabyte)
+// @license         GNU GPL v3 (http://www.gnu.org/copyleft/gpl.html)
+// @homepage        https://github.com/techabyte/
+// @include         http://balkanmp3.ba*
+// @grant           none
+// @require         https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js
 //
-// @version        0.1
-// @history        0.1 first version
-// @history        1.0b first beta version, who knew!!
+// @version         0.1
+// @history         0.1 first version
+// @history         1.0b first beta version, who knew!!
 //
 // ==/UserScript==
 
