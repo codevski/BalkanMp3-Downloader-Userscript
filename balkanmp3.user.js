fcbbbc3443dedb0f7c3495c120b94a2597d4b054
Saso Petrovski <techabyte@users.noreply.github.com>
2016-07-06 16:49:54 +1000

    Cleanup - Cosmetic Only History

--- balkanmp3.user.js
+++ balkanmp3.user.js
@@ -9,10 +9,8 @@
 // @include         http://balkanmp3.ba*
 // @grant           none
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js
-//
 // @version         0.1
-// @history         0.1 first version
-// @history         1.0b first beta version, who knew!!
+// @history         0.1 Cosmetic Only
 //
 // ==/UserScript==
 
