1fe35b572a688e0eadf4441821c10d3d7f44afd1
techabyte <s@saso.io>
2016-07-06 16:37:49 +1000

    add div

---
+++ balkanmp3.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+//
+// @name           balkanmp3 Downloader
+// @description    Easy Balkanmp3 Downloader
+// @namespace      https://github.com/techabyte/
+// @author         techabyte (https://github.com/techabyte)
+// @license        GNU GPL v3 (http://www.gnu.org/copyleft/gpl.html)
+// @homepage       https://github.com/techabyte/
+// @include        http://balkanmp3.ba*
+// @grant       none
+// @require        https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js
+//
+// @version        0.1
+// @history        0.1 first version
+// @history        1.0b first beta version, who knew!!
+//
+// ==/UserScript==
+
+
+//And of course your code!!
+$(function(){
+window.alert("Hello.. My Extension processed you..");
+
+$(".track-buttons-container").append('<div class="share-button" onclick="share(40171, 1)"><span class="action-text">Download</span></div>');
+
+});
